0c6a2e56a5f1ffaf443db334dad0e0a419b2d671
김승범 <kimsb7218@naver.com>
2024-01-06 17:01:20 +0900

    chore: set build.gradle.kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,8 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_17
-}
 
 repositories {
 	mavenCentral()
@@ -29,9 +27,18 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+}
+
+noArg {
+	annotation("jakarta.persistence.Entity")
+}
+
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
 		jvmTarget = "17"
 	}
 }
