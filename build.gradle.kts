28c9c66ab3d99ee5820a4979f8e836ecd1ab19bb
김승범 <kimsb7218@naver.com>
2024-01-07 19:27:39 +0900

    chore: Add a validation Dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,8 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	// https://mvnrepository.com/artifact/org.springframework/spring-webflux
 	implementation("org.springframework:spring-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-validation:3.0.6")
 }
 
 allOpen {
